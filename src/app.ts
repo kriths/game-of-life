7cc2ea6addee7958628c6df472ef72507f614f0a
Thomas Krisch <thomas.krisch+git@gmail.com>
2021-10-30 23:00:04 +0200

    refactor: pull stateful app into own class

---
+++ src/app.ts
@@ -0,0 +1,68 @@
+import Grid from './grid';
+
+const TICK_INTERVAL = 100;
+
+/**
+ * Initialize canvas once when loading the page using the screen's size.
+ * Create the canvas 10% bigger than the screen size and center it so that
+ * it overflows roughly equally off all sides of the page.
+ * This creates the illusion of an infinite canvas because cells might flow in
+ * and out of the visible area.
+ */
+function initCanvas(): HTMLCanvasElement {
+  const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+  const canvasHeight = Math.floor(window.screen.height * 1.1);
+  const canvasWidth = Math.floor(window.screen.width * 1.1);
+  canvas.height = canvasHeight;
+  canvas.width = canvasWidth;
+  canvas.style.height = `${canvasHeight}px`;
+  canvas.style.width = `${canvasWidth}px`;
+  canvas.style.top = `-${canvasHeight * 0.05}px`;
+  canvas.style.left = `-${canvasWidth * 0.05}px`;
+  return canvas;
+}
+
+export default class App {
+  private readonly canvas: HTMLCanvasElement;
+
+  private grid: Grid;
+
+  private nextIteration?: number;
+
+  constructor() {
+    this.canvas = initCanvas();
+    this.grid = new Grid(this.canvas);
+
+    this.tick = this.tick.bind(this);
+    document.addEventListener('keypress', this.onKeypress.bind(this));
+  }
+
+  private onKeypress(e: KeyboardEvent) {
+    switch (e.key.toLowerCase()) {
+      case 'p':
+        if (this.nextIteration) {
+          this.stop();
+        } else {
+          this.tick();
+        }
+        break;
+      case 'r':
+        this.stop();
+        this.grid = new Grid(this.canvas);
+        this.tick();
+        break;
+      default:
+        // No further listeners
+    }
+  }
+
+  private stop() {
+    window.clearTimeout(this.nextIteration);
+    this.nextIteration = null;
+  }
+
+  public tick() {
+    this.grid.tickAndRender();
+    this.nextIteration = window.setTimeout(this.tick, TICK_INTERVAL);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,9 @@
 import './index.css';
-import Grid from './grid';
-
-const TICK_INTERVAL = 100;
+import App from './app';
 
 if (process.env.NODE_ENV === 'development') {
   document.getElementById('debug').style.display = 'block';
 }
 
-// Initialize canvas once when loading the page using the screen's size.
-// Create the canvas 10% bigger than the screen size and center it so that
-// it overflows roughly equally off all sides of the page.
-// This creates the illusion of an infinite canvas because cells might flow in
-// and out of the visible area.
-const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-const canvasHeight = Math.floor(window.screen.height * 1.1);
-const canvasWidth = Math.floor(window.screen.width * 1.1);
-canvas.height = canvasHeight;
-canvas.width = canvasWidth;
-canvas.style.height = `${canvasHeight}px`;
-canvas.style.width = `${canvasWidth}px`;
-canvas.style.top = `-${canvasHeight * 0.05}px`;
-canvas.style.left = `-${canvasWidth * 0.05}px`;
-
-// Application state
-let grid = new Grid(canvas);
-let nextIteration: number | null = null;
-
-function tick() {
-  grid.tickAndRender();
-  nextIteration = window.setTimeout(tick, TICK_INTERVAL);
-}
-
-function stop() {
-  window.clearTimeout(nextIteration);
-  nextIteration = null;
-}
-
-document.addEventListener('keypress', (e) => {
-  switch (e.key.toLowerCase()) {
-    case 'p':
-      if (nextIteration) {
-        stop();
-      } else {
-        tick();
-      }
-      break;
-    case 'r':
-      stop();
-      grid = new Grid(canvas);
-      tick();
-      break;
-    default:
-      // No further listeners
-  }
-});
-
-tick();
+const app = new App();
+app.tick();
